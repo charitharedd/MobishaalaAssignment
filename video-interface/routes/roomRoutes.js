287aa60b0985774044413acf1b939e169c6839ea
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-28 18:27:31 +0530

    Update roomRoutes.js

--- video-interface/routes/roomRoutes.js
+++ video-interface/routes/roomRoutes.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const { createRoom, listRooms, generateToken } = require("../controllers/roomController");
+const router = express.Router();
 
+router.post("/rooms", createRoom);
+router.get("/rooms", listRooms);
+router.post("/rooms/:roomId/token", generateToken);
+
+module.exports = router;

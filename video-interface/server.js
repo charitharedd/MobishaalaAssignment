3004936d374e54e21ab9f9248b48ae278d659342
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-28 18:28:09 +0530

    Update server.js

--- video-interface/server.js
+++ video-interface/server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const roomRoutes = require("./routes/roomRoutes");
+const { PORT, DB_URI } = require("./config");
 
+const app = express();
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Database connected"))
+  .catch((error) => console.log("Database connection error:", error));
+
+// Routes
+app.use("/api", roomRoutes);
+
+// Start Server
+app.listen(PORT, () => console.log(Server running on port ${PORT}));

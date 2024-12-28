cedeba5d3ceeb9a91e4559389c624b7abfa0cb71
charitharedd <129164869+charitharedd@users.noreply.github.com>
2024-12-28 18:24:52 +0530

    Update config.js

--- video-interface/config.js
+++ video-interface/config.js
@@ -1,1 +1,9 @@
 
+require("dotenv").config();
+
+module.exports = {
+  PORT: process.env.PORT || 5000,
+  DB_URI: process.env.DB_URI,
+  HMS_SECRET: process.env.HMS_SECRET,
+  HMS_ACCESS_KEY: process.env.HMS_ACCESS_KEY,
+};
